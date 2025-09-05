c7aa6a79988aa0601405c180215f54e703437c6c
LeonardoMonsuete <leonardo.monsuete@outlook.com>
2025-09-04 22:04:40 -0300

    fix: adjusting approach on building unique data around username and email

--- src/db/migrations/1757005588380-create_users_table.ts
+++ src/db/migrations/1757005588380-create_users_table.ts
@@ -3,17 +3,29 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class CreateUsersTable1757005588380 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            CREATE TABLE users (
-                id SERIAL PRIMARY KEY,
-                username VARCHAR(255) NOT NULL UNIQUE,
-                password VARCHAR(255) NOT NULL,
-                email VARCHAR(255) NOT NULL UNIQUE,
-                status BOOLEAN DEFAULT TRUE,
-                created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-                updated_at TIMESTAMP WITH TIME ZONE,
-                deleted_at TIMESTAMP WITH TIME ZONE,
-                last_login TIMESTAMP WITH TIME ZONE
-            );`);
+      CREATE TABLE users (
+          id SERIAL PRIMARY KEY,
+          name VARCHAR(255) NOT NULL,
+          username VARCHAR(255) NOT NULL,
+          password VARCHAR(255) NOT NULL,
+          email VARCHAR(255) NOT NULL,
+          status BOOLEAN DEFAULT TRUE,
+          created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+          updated_at TIMESTAMPTZ,
+          deleted_at TIMESTAMPTZ,
+          last_login TIMESTAMPTZ
+      );
+    `);
+
+    await queryRunner.query(`
+      CREATE UNIQUE INDEX "IDX_USERNAME_ACTIVE" ON "users" ("username")
+      WHERE deleted_at IS NULL;
+    `);
+
+    await queryRunner.query(`
+      CREATE UNIQUE INDEX "IDX_EMAIL_ACTIVE" ON "users" ("email")
+      WHERE deleted_at IS NULL;
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
